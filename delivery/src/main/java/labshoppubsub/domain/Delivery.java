55c85d6cb69413e52e1456573b48a222bd93a4f1
uEngine-Solutions <help@uengine.org>
2022-09-19 07:40:13 +0000

    commit

--- delivery/src/main/java/labshoppubsub/domain/Delivery.java
+++ delivery/src/main/java/labshoppubsub/domain/Delivery.java
@@ -27,11 +27,13 @@ public class Delivery {
     }
 
     public static void addToDeliveryList(OrderPlaced orderPlaced) {
-        /** Example 1:  new item 
+        /** Example 1:  new item
+        */ 
         Delivery delivery = new Delivery();
+        delivery.setCustomerId(orderPlaced.getCustomerId());
+        delivery.setOrderId(orderPlaced.getId());
         repository().save(delivery);
 
-        */
 
         /** Example 2:  finding and process
         
